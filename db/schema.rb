5da34ad945f6a90d6672d34877d7fe13e7d6c7cc
takao0206 <takao.haruna.0206@gmail.com>
2024-10-14 23:07:33 +0900

    Added the feature to add comments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_14_081625) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_14_133920) do
+  create_table "comments", force: :cascade do |t|
+    t.string "user_name"
+    t.text "body"
+    t.integer "idea_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["idea_id"], name: "index_comments_on_idea_id"
+  end
+
   create_table "ideas", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_14_081625) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "ideas"
 end
